d616dc129c0ae7f9bc2b76c9da1aff22c9b08fe1
Bhavik Patel <bhavikpate2010@gmail.com>
2025-11-04 15:28:07 +0530

    fix again layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import ConditionalLayout from "@/components/ConditionalLayout";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -112,13 +110,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
       > 
-      <div className="min-h-screen bg-[#2C3E3E]">
-     
-      <Header />
-      
-        {children}
-         <Footer />
-    </div>
+        <ConditionalLayout>
+          {children}
+        </ConditionalLayout>
       </body>
     </html>
   );
