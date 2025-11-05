942cfc7f729983cefe0917f127c86e584a0b24b5
rushaliprogrammer95 <programmer95.dynamicdreamz@gmail.com>
2025-11-05 15:22:13 +0530

    date issue fix

--- components/ReviewList.tsx
+++ components/ReviewList.tsx
@@ -33,13 +33,23 @@ const formatTimeAgo = (dateString: string) => {
   const date = new Date(dateString);
   const now = new Date();
   const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+  
   let interval = seconds / 31536000;
   if (interval > 1) return Math.floor(interval) + " years ago";
+  
   interval = seconds / 2592000;
   if (interval > 1) return Math.floor(interval) + " months ago";
+  
   interval = seconds / 86400;
   if (interval > 1) return Math.floor(interval) + " days ago";
-  return "Today";
+  
+  interval = seconds / 3600;
+  if (interval > 1) return Math.floor(interval) + " hours ago";
+  
+  interval = seconds / 60;
+  if (interval > 1) return Math.floor(interval) + " minutes ago";
+  
+  return "Just now";
 };
 
 const renderStars = (rating: number) => {
