4b98be1def610f4b21b018ad08aa3714b020d5d0
Tarun Makode <info@dynamicdreamz.com>
2025-12-17 11:53:45 +0530

    project done 5 pages + forgot password

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -4,12 +4,14 @@ import { useState } from 'react'
 import { supabase } from '@/lib/supabase'
 import AuthCard from '@/components/AuthCard'
 import { useRouter } from 'next/navigation'
-import { Lock } from 'lucide-react'
 
 export default function ResetPasswordPage() {
   const [password, setPassword] = useState('')
   const [confirm, setConfirm] = useState('')
-  const [show, setShow] = useState(false)
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirm, setShowConfirm] = useState(false)
+
   const router = useRouter()
 
   async function submit() {
@@ -19,6 +21,7 @@ export default function ResetPasswordPage() {
     }
 
     const { error } = await supabase.auth.updateUser({ password })
+
     if (error) {
       alert(error.message)
       return
@@ -29,22 +32,22 @@ export default function ResetPasswordPage() {
 
   return (
     <AuthCard title="Set New Password">
-      
-      {/* NEW PASSWORD */}
+
+      {/* ================= NEW PASSWORD ================= */}
       <div className="relative mt-5 mb-5">
         <label className="block text-left text-[14px] text-[#20231E] font-medium mb-2">
           New Password
         </label>
+
         {/* LOCK ICON */}
-        {/* <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[#6B6F63]" /> */}
         <img
-            src={show ? '/lock.svg' : '/lock.svg'}
-            alt="lock icon"
-            className="absolute left-3 top-10.5 w-4 h-4 text-[#6B6F63]"
-          />
+          src="/lock.svg"
+          alt="lock icon"
+          className="absolute left-3 top-10.5 w-4 h-4"
+        />
 
         <input
-          type={show ? 'text' : 'password'}
+          type={showPassword ? 'text' : 'password'}
           placeholder="New Password"
           className="
             w-full h-[44px]
@@ -64,32 +67,32 @@ export default function ResetPasswordPage() {
         {/* EYE ICON */}
         <button
           type="button"
-          onClick={() => setShow(!show)}
+          onClick={() => setShowPassword(!showPassword)}
           className="absolute right-3 top-10.5"
         >
           <img
-            src={show ? '/eye-close.svg' : '/eye-line.svg'}
+            src={showPassword ? '/eye-close.svg' : '/eye-line.svg'}
             alt="toggle password visibility"
             className="w-4 h-4"
           />
         </button>
       </div>
 
-      {/* CONFIRM PASSWORD */}
+      {/* ================= CONFIRM PASSWORD ================= */}
       <div className="relative">
         <label className="block text-left text-[14px] text-[#20231E] font-medium mb-2">
           Confirm Password
         </label>
+
         {/* LOCK ICON */}
-        {/* <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[#6B6F63]" /> */}
         <img
-            src={show ? '/lock.svg' : '/lock.svg'}
-            alt="lock icon"
-            className="absolute left-3 top-10.5 w-4 h-4 text-[#6B6F63]"
-          />
+          src="/lock.svg"
+          alt="lock icon"
+          className="absolute left-3 top-10.5 w-4 h-4"
+        />
 
         <input
-          type={show ? 'text' : 'password'}
+          type={showConfirm ? 'text' : 'password'}
           placeholder="Confirm Password"
           className="
             w-full h-[44px]
@@ -105,23 +108,24 @@ export default function ResetPasswordPage() {
           value={confirm}
           onChange={(e) => setConfirm(e.target.value)}
         />
+
         {/* EYE ICON */}
         <button
           type="button"
-          onClick={() => setShow(!show)}
+          onClick={() => setShowConfirm(!showConfirm)}
           className="absolute right-3 top-10.5"
         >
           <img
-            src={show ? '/eye-close.svg' : '/eye-line.svg'}
-            alt="toggle password visibility"
+            src={showConfirm ? '/eye-close.svg' : '/eye-line.svg'}
+            alt="toggle confirm password visibility"
             className="w-4 h-4"
           />
         </button>
       </div>
 
       {/* HELPER TEXT */}
-      <p className="text-[12px] text-[#41463B] font-normal leading-[20px] text-left max-w-[297px] mt-2">
-          Minimum 8 character, at least one special character, one number
+      <p className="text-[12px] text-[#41463B] leading-[20px] text-left max-w-[297px] mt-2">
+        Minimum 8 characters, at least one special character and one number
       </p>
 
       {/* SUBMIT */}
@@ -134,16 +138,14 @@ export default function ResetPasswordPage() {
           rounded-full
           text-[16px]
           font-semibold
-          disabled:opacity-60
           hover:bg-[#5A5E4F]
-          cursor-pointer
           mt-5
-          mx-auto
-          transition: background-color 0.3s ease-in-out
+          transition
         "
       >
         Set New Password
       </button>
+
     </AuthCard>
   )
 }
