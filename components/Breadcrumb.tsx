d69d448de7ec6e735e7eca9faf4147cce5f16470
Dynamic <support@dynamicdreamz.com>
2026-02-05 15:46:01 +0530

    UI Changes

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -17,17 +17,28 @@ interface BreadcrumbProps {
 export default function Breadcrumb({ items, className = "" }: BreadcrumbProps) {
   return (
     <div className={`bg-[#F5F1E8] ${className}`}>
-      <div className="max-w-7xl mx-auto px-6 py-3">
-        <div className="flex items-center gap-2 text-sm text-gray-600">
+      <div className="max-w-7xl mx-auto sm:px-6 px-3 pt-3 sm:pb-6.5 pb-4">
+        <div className="flex items-center gap-2 sm:text-base text-sm text-[#41463B]">
           {items?.map((item, index) => (
             <React.Fragment key={index}>
               {index > 0 && <span>/</span>}
+
               {item.href && !item.isActive ? (
-                <Link href={item.href} className="text-[#41463B] hover:text-[#0B0C0A] underline font-[16px] transition-all duration-500">
+                <Link
+                  href={item.href}
+                  className={`text-[#41463B] hover:text-[#0B0C0A] transition-all duration-500 ${index === 0 ? "underline" : "no-underline"
+                    }`}
+                >
                   {item.label}
                 </Link>
               ) : (
-                <span className={item.isActive ? "text-[#0B0C0A] font-[500]" : "text-[#41463B]"}>
+                <span
+                  className={
+                    item.isActive
+                      ? "text-[#0B0C0A] font-medium"
+                      : "text-[#41463B]"
+                  }
+                >
                   {item.label}
                 </span>
               )}
